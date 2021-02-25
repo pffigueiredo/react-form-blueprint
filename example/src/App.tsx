6eba902a189d32910051748fc2583f5321323de1
Pedro Figueiredo <klisarkk@gmail.com>
2021-02-25 11:47:07 +0000

    refactor: update example to new init API

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,29 +1,33 @@
 import React from 'react';
 import { getElementInputs, initForm } from 'react-formgen';
 
-initForm({ text: <input className="text" />, number: <input className="number" /> });
-
 interface Person {
   firstName: string;
   lastName: string;
   age: number;
 }
 
-const App = () => {
-  const formControls = getElementInputs<Person>([
-    {
-      type: 'text',
-      name: 'firstName',
-    },
-    { type: 'number', name: 'age' },
-  ]);
+initForm({ text: <input className="text" />, number: <input className="number" /> });
+const formControls = getElementInputs<Person>([
+  {
+    type: 'text',
+    name: 'firstName',
+  },
+  {
+    type: 'text',
+    name: 'lastName',
+  },
+  { type: 'number', name: 'age' },
+]);
 
+const App = () => {
   if (!formControls) return null;
 
   return (
     <>
       <formControls.firstName />
-      <formControls.age />
+      <formControls.lastName />
+      <formControls.age ref={'cenas'} />
     </>
   );
 };
